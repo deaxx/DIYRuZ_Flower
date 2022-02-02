5030a5e830310e185392db66324213babde1d567
Denis Gorbunov <deaxx13@gmail.com>
2022-02-03 00:35:06 +0500

    Add files via upload

---
+++ DIYRuZ_Flower/Source/version.c
@@ -0,0 +1,18 @@
+
+    #ifndef ZCL_APP_VERSION_H
+    #define ZCL_APP_VERSION_H
+
+    #ifdef __cplusplus
+    extern "C" {
+    #endif
+
+    #include "version.h"
+const uint8 zclApp_DateCode[] = { 16, '0', '3', '/', '0', '2', '/', '2', '0', '2', '2', ' ', '0', '0', ':', '1', '8' };
+const char zclApp_DateCodeNT[] = "03/02/2022 00:18";
+
+    #ifdef __cplusplus
+    }
+    #endif
+
+    #endif /* ZCL_APP_VERSION_H */
+    
